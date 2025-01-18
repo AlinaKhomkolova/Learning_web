c7656bd2e9031145ec980fb505ffd8432aa99ee8
alina <khomkolovaalina@gmail.com>
2025-01-18 18:51:34 +0100

    Добавила новую модель Pay в приложение users

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from config.settings import NULLABLE
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -42,3 +43,61 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Pay(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='Пользователь',
+        help_text='Пользователь, который осуществил покупку.'
+    )
+    data_pay = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата оплаты',
+        help_text='Дата и время совершения оплаты.'
+    )
+    pay_course = models.ForeignKey(
+        Course,
+        **NULLABLE,
+        on_delete=models.CASCADE,
+        related_name='payments_for_course',
+        verbose_name='Оплаченный курс',
+        help_text='Курс, за который была произведена оплата.'
+    )
+    pay_lesson = models.ForeignKey(
+        Lesson,
+        **NULLABLE,
+        on_delete=models.CASCADE,
+        related_name='payments_for_lesson',
+        verbose_name='Оплаченный урок',
+        help_text='Урок, за который была произведена оплата.'
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Сумма оплаты',
+        help_text='Сумма, которую заплатил пользователь.'
+    )
+
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        default='cash',
+        verbose_name='Способ оплаты',
+        help_text='Способ, с помощью которого была произведена оплата.'
+    )
+
+    def __str__(self):
+        return f"Оплата {self.amount} за {self.pay_course or self.pay_lesson}"
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
